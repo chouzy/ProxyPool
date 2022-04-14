6dd9fdeb3de81b55dd00ec955708e7a2b72431e7
chou <1946712450@qq.com>
2022-04-14 15:17:18 +0800

    submit V1.1.1

--- proxypool/main.py
+++ proxypool/main.py
@@ -2,9 +2,11 @@ import multiprocessing
 import time
 
 from loguru import logger
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.wsgi import WSGIContainer
 
-from config.setting import IS_WINDOWS, START_TESTER, START_GETTER, START_SERVER, CYCLE_TESTER, CYCLE_GETTER, API_HOST, \
-    API_PORT
+from config.setting import IS_WINDOWS, START_TESTER, START_GETTER, START_SERVER, CYCLE_TESTER, CYCLE_GETTER, API_PORT
 from proxypool.service.getter import Getter
 from proxypool.service.server import app
 from proxypool.service.tester import Tester
@@ -62,7 +64,9 @@ class ProxyPoolMain(object):
         if not START_SERVER:
             logger.info('server not enabled, exit')
             return
-        app.run(host=API_HOST, port=API_PORT)
+        s = HTTPServer(WSGIContainer(app))
+        s.listen(API_PORT)
+        IOLoop.current().start()
 
     def run(self):
         global tester_process, getter_process, server_process
@@ -83,16 +87,16 @@ class ProxyPoolMain(object):
                 logger.info(f'starting server, pid {server_process.pid}...')
                 server_process.start()
 
-            tester_process and tester_process.join()
-            getter_process and getter_process.join()
-            server_process and server_process.join()
+            tester_process.join()
+            getter_process.join()
+            server_process.join()
         except Exception as e:
             logger.error(f'an exception occurred in the proxypool, and err message is: {e}')
         finally:
             # must call join method before calling is_alive
-            tester_process and tester_process.join()
-            getter_process and getter_process.join()
-            server_process and server_process.join()
+            tester_process.join()
+            getter_process.join()
+            server_process.join()
             logger.info(
                 f'tester is {"alive" if START_TESTER and tester_process.is_alive() else "dead"}')
             logger.info(
